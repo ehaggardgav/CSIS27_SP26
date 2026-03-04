f3db36956e07df28d8551938e2a18e3e22ba440c
Emmet Haggard <143468532+ehaggardgav@users.noreply.github.com>
2026-03-03 18:17:57 -0800

    Added average calculation method and notes

--- Wk_1/AvgMethod.java
+++ Wk_1/AvgMethod.java
@@ -3,6 +3,16 @@ import java.util.LinkedList;
 
 public class AvgMethod {
     public static void main(String [] args) {
-    }    
+    }  
+    
+    /**
+    * Calculates the average of integers in an ArrayList
+    * Takes as input values from the list of integers, "list"
+    * Returns value in double format
+    * Returns 0.0 if the list is empty
+    */
+    public static double average(ArrayList<Integer> list) { 
+
+    }  
     
 }
