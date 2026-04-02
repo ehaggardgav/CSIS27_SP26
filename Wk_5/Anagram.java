5cec27796db089b6aa34f802ea0587d5733bc56f
Emmet Haggard <143468532+ehaggardgav@users.noreply.github.com>
2026-04-02 00:11:05 -0700

    Saved most updated version to continue later

--- Wk_5/Anagram.java
+++ Wk_5/Anagram.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class Anagram {
 
 public static void main (String[] args) {
+    
+    //user input
     Scanner input = new Scanner(System.in);
 
-    System.out.print("Enter word 1: ");
+    System.out.print("Enter word 1: "); 
     String word1 = input.nextLine();
 
     System.out.print("Enter word 2: ");
@@ -20,6 +22,23 @@ public static void main (String[] args) {
     }
 
     input.close();
+}
+
+public static boolean isAnagram(String, str1, String str2) {
+    //check for different word lengths first
+    if (str1.length() != str2.length()) {
+        return false;
+    }
+
+    //now convert strings to char arrays
+    char [] arr1 = str1.toCharArray();
+    char [] arr2 = str2.toCharArray();
+
+    //recursive sorting for each array
+    selectionSort(arr1, 0);
+    selectionSort(arr2, 0);
+
+    return arraysEqual(arr1, arr2, 0);
 }
     
 }
