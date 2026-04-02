b7b97fb570dda4da031d5c6e6511fcca91329d20
Emmet Haggard <143468532+ehaggardgav@users.noreply.github.com>
2026-04-01 23:26:29 -0700

    Add scanner input

--- Wk_5/Anagram.java
+++ Wk_5/Anagram.java
@@ -1,5 +1,23 @@
 import java.util.Scanner;
 
 public class Anagram {
+
+public static void main (String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter word 1: ");
+    String word1 = input.nextLine();
+    String word2 = input.nextLine();
+    
+    boolean result = isAnagram(word1, word2);
+
+    if (result) {
+        System.out.println("The words are anagrams.");
+    } else {
+        System.out.println("The words are NOT anagrams.");
+    }
+
+    input.close();
+}
     
 }
