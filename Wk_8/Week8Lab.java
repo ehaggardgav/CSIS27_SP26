202dbae703ef42e08c6b2b37df0c36edeff04ffd
Emmet Haggard <143468532+ehaggardgav@users.noreply.github.com>
2026-04-28 22:58:42 -0700

    Add hashCode() function

--- Wk_8/Week8Lab.java
+++ Wk_8/Week8Lab.java
@@ -1,3 +1,13 @@
 public class Week8Lab {
-    
+    public static void main(String[] args) {
+
+    }
+
+   public int hashCode() {
+        int h = 0;
+        for (int i = 0; i < first.length(); i++) {
+            h = h + first.charAt(i);
+        }
+        return h;
+   } 
 }
