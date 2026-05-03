20f3aa78af510b93a009a1a5b5d57f42d20cde7b
Emmet Haggard <143468532+ehaggardgav@users.noreply.github.com>
2026-05-02 19:07:36 -0700

    Added classes

--- Wk_9/Week9Lab.java
+++ Wk_9/Week9Lab.java
@@ -1,3 +1,25 @@
 public class Week9Lab {
-    
+    public static void main(String[] args) {
+
+    }
+
+    public int minDepth(TreeNode root) {
+
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+
+        TreeNode(int val) { this.val = val; }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
