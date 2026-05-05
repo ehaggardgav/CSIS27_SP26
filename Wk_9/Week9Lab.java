1879b4d162bdf628d921da4d4b7107cba0dc0fcc
Emmet Haggard <143468532+ehaggardgav@users.noreply.github.com>
2026-05-04 22:36:13 -0700

    Added code to build the tree

--- Wk_9/Week9Lab.java
+++ Wk_9/Week9Lab.java
@@ -57,6 +57,27 @@ public class Week9Lab {
         return root;
     }
 
+    public static void printTree(TreeNode root) {
+        if (root == null) return;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode current = queue.poll();
+                System.out.print(current.val + " ");
+
+                if (current.left != null) queue.add(current.left);
+                if (current.right != null) queue.add(current.right);
+            }
+
+            System.out.println();
+        }
+    }
+
     public int minDepth(TreeNode root) {
         if (root == null) return 0;
 
